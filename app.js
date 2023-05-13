a512478f1bcad4e2bc4623e0231f656e4d660d36
MaryanDehv <maryanmodev@gmail.com>
2023-05-13 16:06:10 +0100

    projects animation;

--- app.js
+++ app.js
@@ -10,3 +10,33 @@ function mobileNav(){
 
 
 mobileNav()
+
+
+ // portfolio deck
+ function portfolioDeckEvents(){
+    const directions = document.querySelectorAll('.directions > div');
+
+    directions.forEach((direction) => {            
+         direction.addEventListener('click' , (el) => {
+            console.log('clicked ')
+            const getCards = document.querySelectorAll('.project-card');
+            const cardsParent = document.querySelector('.projects-container');
+            console.log(el.currentTarget)
+             if(el.currentTarget.classList.contains('next')){
+                addClasses(getCards , cardsParent)
+                cardsParent.insertBefore(getCards[0], document.querySelector('directions'));
+             } else if(el.currentTarget.classList.contains('prev')){
+                cardsParent.insertBefore(getCards[getCards.length - 1], getCards[0])
+                addClasses(getCards)
+             }
+         })
+     })
+
+     function addClasses(element){
+        element[0].setAttribute('class' ,  'project-card last moveback');
+        element[1].setAttribute('class' ,  'project-card shiftup');
+        element[2].setAttribute('class' , 'project-card second lastshiftup');
+     }
+  }
+
+  portfolioDeckEvents()
