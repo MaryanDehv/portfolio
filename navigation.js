c131a1b75f6cc1726aee8cb4dc448e50b3439dcf
MaryanDehv <maryanmodev@gmail.com>
2023-05-31 10:08:25 +0100

    nav

---
+++ navigation.js
@@ -0,0 +1,12 @@
+function mobileNav(){
+    const navIcon = document.querySelectorAll(".nav-icons div");
+    const navParent = document.querySelector('[data-mobile-nav]');
+    navIcon.forEach(icon => {
+        icon.addEventListener('click' , (el) => {
+            navParent.setAttribute('data-mobile-nav' , navParent.getAttribute('data-mobile-nav') == 'true' ? !true : !false)
+        })
+    })
+}
+
+
+mobileNav()
