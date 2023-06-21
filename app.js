6c9f911e64a157d964292bfdb5ae7bc1bfe62126
MaryanDehv <maryanmodev@gmail.com>
2023-06-21 16:08:41 +0100

    working on directions animation

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 function portfolioDeckEvents(){
    console.log('portfolio')
    const directions = document.querySelectorAll('.directions > div');
+   let animationStop;
 
    directions.forEach(direction => {
-      direction.addEventListener('click' , () => {
-         const currentList = getCards();
-         const getFirst = getCards()[0];
-         const container = document.querySelector('.projects-list')
-         container.appendChild(getFirst)
-         currentList[0].setAttribute('class' ,  'project-card moveback');
-         currentList[2].setAttribute('class' ,  'project-card shiftup');
-         currentList[currentList.length - 1].setAttribute('class' , 'project-card lastshiftup');
+      direction.addEventListener('click' , (el) => {
+         resetMoveClasses()
+         const container = document.querySelector('.projects-list');
+         if(el.target.classList.contains('next')){
+            getCards()[0].classList.add('moveback')
+            getCards()[1].classList.add('shiftup')
+            getCards()[getCards().length - 1].classList.add('lastshiftup')
+            container.appendChild(getCards()[0])
+         } else if(el.target.classList.contains('prev')){
+            getCards()[0].classList.add('shiftup' , 'reverse')
+            getCards()[1].classList.add('lastshiftup' , 'reverse')
+            getCards()[getCards().length - 1].classList.add('moveback' , 'reverse')
+            container.insertBefore(getCards()[getCards().length - 1] , getCards()[0])
+         }
       })
    })
 }
 
 function getCards(){
    return document.querySelectorAll('.project-card');
+}
+
+function resetMoveClasses(){
+   getCards().forEach(card => card.setAttribute('class' ,  'project-card'));
 }
 
 portfolioDeckEvents()
